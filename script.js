b8d6c23a29cb8c3fb535606322cb2acf6ccdd68e
M <114847810+riraum@users.noreply.github.com>
2024-09-11 19:10:02 +0900

    Initialize project with files

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Initialize");
+// Create 16x16 grid squares inside div
+// div with id="container" exists
+// const gridSquares = document.createElement("div");
+// const divSelect = document.querySelector("#container");
+// divSelect.appendChild(gridSquares);
