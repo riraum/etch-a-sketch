4ad90164f82fd2204622c6dbbd44e9af34c6e01e
M <114847810+riraum@users.noreply.github.com>
2024-09-12 11:49:51 +0900

    Add divs through loop

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 console.log("Initialize");
 // Create 16x16 grid squares inside div
 // div with id="container" exists
-// const gridSquares = document.createElement("div");
-// const divSelect = document.querySelector("#container");
-// divSelect.appendChild(gridSquares);
+const divSelect = document.querySelector("#container");
+for (let i = 0; i < 32; i++) {
+  let gridSquares = document.createElement("div");
+  divSelect.appendChild(gridSquares);
+  gridSquares.style.width = "100px";
+  gridSquares.style.height = "100px";
+  gridSquares.style.background = "red";
+}
